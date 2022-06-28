c75703d1748c1ec698a8489ec44988b3b1953029
NikSavc <58150480+NikSavchenk0@users.noreply.github.com>
2022-06-28 19:49:32 +0300

    Add files via upload

---
+++ support/systemstatus/localizer.js
@@ -0,0 +1,12 @@
+( function() {
+	var localizer = {
+		locale: "en_US",
+		title: "System Status",
+		timeFormat : " hh:mm a ",
+		supportText: "If you are experiencing an issue not listed here, ",
+		supportLink: "contact support",
+		developerText: "Looking for developer system status?",
+		developerLink: "Find it here"
+	}
+	window.sspLocalizer = localizer;
+})();
